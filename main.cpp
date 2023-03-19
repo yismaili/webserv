502f55243ef3ae91842f08fb29f8df5850f50e33
Younes Ismaili <ismailiyounes20@gmail.com>
2023-03-19 11:38:15 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -3,19 +3,71 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: yismaili <yismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 19:21:09 by yismaili          #+#    #+#             */
-/*   Updated: 2023/01/29 19:21:13 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/03/19 11:35:09 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+const int PORT = 8080;
 
 int main() {
+    int sockfd, newsockfd, n;
+    char buffer[4096];
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
 
- std:: cout << "Hello ft_containers" << std:: endl;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Error opening socket" << std::endl;
+        return 1;
+    }
 
-  return 0;
+    std::memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
 
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Error binding socket" << std::endl;
+        return 1;
+    }
+
+    listen(sockfd, 5);
+
+    while (true) {
+        clilen = sizeof(cli_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            std::cerr << "Error accepting connection" << std::endl;
+            continue;
+        }
+
+        std::ostringstream response;
+        response << "HTTP/1.1 200 OK\r\n";
+        response << "Content-Type: text/html; charset=UTF-8\r\n";
+        response << "\r\n";
+        response << "<html><body><h1>Hello younes from HTTP server!</h1></body></html>";
+
+        std::string response_str = response.str();
+        n = write(newsockfd, response_str.c_str(), response_str.length());
+        if (n < 0) {
+            std::cerr << "Error writing to socket" << std::endl;
+        }
+
+        close(newsockfd);
+    }
+
+    close(sockfd);
+    return 0;
 }
