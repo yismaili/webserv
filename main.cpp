c7644ea1d703e3f4821dacdff8ec5529fca40a0b
Younes Ismaili <ismailiyounes20@gmail.com>
2023-03-20 14:56:20 +0000

    add makefile and make small http server

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 19:21:09 by yismaili          #+#    #+#             */
-/*   Updated: 2023/03/19 12:12:39 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/03/20 14:55:41 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,42 +18,62 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+
 class HttpServer {
 public:
     HttpServer(int port) : sockfd(-1), port(port) {}
-
     bool Start() {
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        
+// Socket System Call
+
+        //Creates a socket and returns a Socket Descriptor (like file descriptor) which is an integer value
+        sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+        
+        //domain: specifies the communication domain, such as AF_INET for IPv4 or AF_INET6 for IPv6.
+        //type: specifies the type of socket, such as SOCK_STREAM for a TCP socket or SOCK_DGRAM for a UDP socket.
+        /*protocol: specifies the protocol to be used with the socket, such as IPPROTO_TCP for TCP or IPPROTO_UDP for UDP. 
+        This argument is usually set to 0,which allows the operating system to choose the 
+        appropriate protocol based on the socket type and domain.*/
+        
         if (sockfd < 0) {
             std::cerr << "Error opening socket" << std::endl;
             return false;
-        }
-
+        }  
         bzero((char *) &serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
-        serv_addr.sin_port = htons(port);
+        // AF stands for Address Family and PF stands for Protocol Family
+       // This construct holds the information about the address family, port number, Internet address
+        serv_addr.sin_family = AF_INET; // Address family // IPv4 Internet protocols    
+        serv_addr.sin_addr.s_addr = INADDR_ANY;  // Internet address
+        serv_addr.sin_port = htons(port); // Port number // Network to Host Shor
 
+// Bind System Call
+        //associate a socket with a specific address and port number
         if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+            //the second is a pointer to a struct sockaddr structure that contains the address
             std::cerr << "Error binding socket" << std::endl;
             return false;
         }
-
+//Listen() System Call
+        //prepares a connection-oriented server to accept client connections.
         listen(sockfd, 5);
-
+        //The second parameter specifies the number of requests that the system queues before it executes the accept()
         return true;
     }
 
     void Run() {
         while (true) {
+            //structure that is used to store the information about the client's address
             struct sockaddr_in cli_addr;
+            //represent the length of a socket address
             socklen_t clilen = sizeof(cli_addr);
+            // Accepts a connection on a socket.
             int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
             if (newsockfd < 0) {
                 std::cerr << "Error accepting connection" << std::endl;
                 continue;
             }
 
+            // Insert html page
             std::ostringstream response;
             response << "HTTP/1.1 200 OK\r\n";
             response << "Content-Type: text/html; charset=UTF-8\r\n";
@@ -61,7 +81,8 @@ public:
             response << "<html><body><h1>Hello younes from HTTP server!</h1></body></html>";
 
             std::string response_str = response.str();
-            int n = write(newsockfd, response_str.c_str(), response_str.length());
+           // send the contents of the object response_str over a network connection.
+            int n = write(newsockfd, response_str.c_str(), response_str.length());// converted to a C-style string 
             if (n < 0) {
                 std::cerr << "Error writing to socket" << std::endl;
             }
