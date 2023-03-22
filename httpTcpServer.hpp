6646655fbca2b2b0a68b24dd1fbed01b9bfd8eff
Younes Ismaili <ismailiyounes20@gmail.com>
2023-03-22 23:14:18 +0000

    fix read of requist

--- httpTcpServer.hpp
+++ httpTcpServer.hpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:57:52 by yismaili          #+#    #+#             */
-/*   Updated: 2023/03/22 19:51:02 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/03/22 22:53:53 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,21 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include<fstream>
+#include <arpa/inet.h>
 
 #define num_of_req 20;
 namespace http{
     class tcpServer{
         public:
-         tcpServer(int port_) : sockfd(-1), port(port_),  newsockfd(),sock_addr_len(0) {
+         tcpServer(int port_, std::string ip_add) : sockfd(-1), port(port_),  newsockfd(),sock_addr_len(0), ip_addr(ip_add){
             // AF stands for Address Family and PF stands for Protocol Family
             // This construct holds the information about the address family, port number, Internet address
             serv_addr.sin_family = AF_INET; // Address family // IPv4 Internet protocols    
-            serv_addr.sin_addr.s_addr = INADDR_ANY;  // Internet "address inet_addr(ip_addr.c_str());"
+            serv_addr.sin_addr.s_addr = inet_addr(ip_addr.c_str());  // Internet "address inet_addr(ip_addr.c_str());"
             serv_addr.sin_port = htons(port); // Port number // Network to Host Shor
             if(start_server() == false){
-                printmessage("Failed to start server ");
+                print_message("Failed to start server ");
             }
          }
          ~tcpServer(){
@@ -48,15 +50,14 @@ namespace http{
             /*protocol: specifies the protocol to be used with the socket, such as IPPROTO_TCP for TCP or IPPROTO_UDP for UDP. 
             This argument is usually set to 0,which allows the operating system to choose the 
             appropriate protocol based on the socket type and domain.*/
-            
             if (sockfd < 0) {
-                return false;
+                return (false);
             }  
     // Bind System Call
             //associate a socket with a specific address and port number
             if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
                 //the second is a pointer to a struct sockaddr structure that contains the address
-                return false;
+                return (false);
             }
             return true;
     }
@@ -65,19 +66,29 @@ namespace http{
         // Accepts a connection on a socket.
        newsockfd = accept(sockfd, (struct sockaddr *) &serv_addr, &sock_addr_len);
         if (newsockfd < 0) {
-            exitWithError(" accepting connection");
+            exit_withError(" accepting connection");
         }
     }
     
     std::string build_response(){
          // Insert html page or ...
-            std::ostringstream response;
+            std::ostringstream response; //create the output string stream
             response << "HTTP/1.1 200 OK\r\n";
             response << "Content-Type: text/html; charset=UTF-8\r\n";
             response << "\r\n";
             response << "<html><body><h1>Hello younes </h1>";
             response << "<h1>from HTTP server!</h4>";
             response << "</body></html>";
+            // std::ifstream file("index.html"); // open the HTML file
+            // if (file) { // check if the file is open
+            //     response << file.rdbuf(); // copy the file contents to the string stream
+            //     file.close(); // close the file
+            // } else {
+            //     std::cerr << "Failed to open file." << std::endl;
+            //     return (response.str()); // exit the program with an error status
+            // }
+            // print the contents of the string stream to the console
+            // std::cout << response.str() << std::endl;
             std::string response_str = response.str();
             return (response_str);
     }
@@ -87,9 +98,9 @@ namespace http{
          std::string response = build_response();
         byte_send = send(newsockfd, response.c_str(), response.length(), 0);
         if (byte_send > 0){
-            printmessage("Server Response sent to client");
+            print_message("Server Response sent to client");
         }else{
-            printmessage("Error: sending response to client");
+            print_message("Error: sending response to client");
         }
     }
     
@@ -98,11 +109,11 @@ namespace http{
         //prepares a connection-oriented server to accept client connections.
         if (listen(sockfd, 5) < 0){
         //The second parameter specifies the number of requests that the system queues before it executes the accept()
-            exitWithError("Socket listen failed");
+            exit_withError("Socket listen failed");
         }
-        printmessage(" Listening on adress ... ");
+        print_message(" Listening on adress ... ");
         while (true) {
-            printmessage("Waiting for a new connection ...");
+            print_message("Waiting for a new connection ...");
             accept_connection();
             read_request();
             send_response();
@@ -115,20 +126,20 @@ namespace http{
         char buffer[1024];
         int  bytes_received = read(newsockfd, buffer, sizeof(buffer));
         if (bytes_received == -1) {
-            exitWithError("Failed to read from socket");
             close(newsockfd);
+            exit_withError("Failed to read from socket");
         }
         // Parse incoming request data
         std::string request_str(buffer, bytes_received);
         std::cout<<request_str<<std::endl;
     }
     
-    void printmessage(const std::string &message){
+    void print_message(const std::string &message){
             std::cout << message << std::endl;
     }
         
-    void exitWithError(const std::string &errormessage){
-        printmessage("ERROR: " + errormessage);
+    void exit_withError(const std::string &errormessage){
+        print_message("ERROR: " + errormessage);
         exit(1);
     }
 
@@ -145,8 +156,7 @@ namespace http{
         struct sockaddr_in serv_addr;
         std::string serv_message;
         unsigned int sock_addr_len;
-        std::string ip_addr;
-        
+        std::string ip_addr;   
     };
 }
 
--- main.cpp
+++ main.cpp
@@ -6,14 +6,20 @@
 /*   By: yismaili <yismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 19:21:09 by yismaili          #+#    #+#             */
-/*   Updated: 2023/03/22 19:25:50 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/03/22 22:47:28 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "httpTcpServer.hpp"
 
 int main() {
-    http::tcpServer server(8080);
-    server.run();
+    http::tcpServer server1(8080, "0.0.0.0");
+    //  http::tcpServer server2(80, "0.0.0.0");
+    //   http::tcpServer server3(90, "0.0.0.0");
+    //    http::tcpServer server4(160, "0.0.0.0");
+    server1.run();
+    //  server2.run();
+    //   server3.run();
+    //    server4.run();
     return 0;
 }
