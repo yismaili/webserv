3785ee5e45ca3fa0bbf0cf708e112a70e6ccf90c
Younes Ismaili <ismailiyounes20@gmail.com>
2023-03-30 18:47:48 +0000

    rendre ce serveur compatible avec plusieurs clients

--- svr_rsc/http_server.hpp
+++ svr_rsc/http_server.hpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:57:52 by yismaili          #+#    #+#             */
-/*   Updated: 2023/03/30 17:00:58 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/03/30 18:44:41 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,35 @@
 #include <vector>
 #include "tcpServer.hpp"
 #include <iterator>
- #include <sys/select.h>
+#include <sys/select.h>
 
-#define num_of_req 20;
 namespace http{
     class http_sever{
         public:
             http_sever(std::vector<int> port_, std::string ip_add) :tcp(){
                 int i = 0;
                 std::vector<int>::iterator it = port_.begin();
-            while (it < port_.end()){
-                    other_sock.push_back(tcp.init_data(*it, ip_add));
-                    std::cout<<other_sock.begin()->sockfd<<std::endl;
-                    it++;
-                } 
+                
+                while (it < port_.end()){
+                        other_sock.push_back(tcp.init_data(*it, ip_add));
+                        std::cout<<other_sock.begin()->sockfd<<std::endl;
+                        it++;
+                }
             }
             
             ~http_sever(){
-            std::map<int, http::tcpServer>::iterator it = sock_inf.begin();
-            while (it != sock_inf.end()){
-                    closeServer(it->first);
-                    it++;
-            }
+                std::map<int, http::tcpServer>::iterator it = sock_inf.begin();
+                
+                while (it != sock_inf.end()){
+                        closeServer(it->first);
+                        it++;
+                }
             }
          
             int accept_connection(int sockfd){
                 // Accepts a connection on a socket.
                 int newsockfd;
+                
                 newsockfd = accept(sockfd, (struct sockaddr *) &tcp.serv_addr, &tcp.sock_addr_len);
                 sock_inf.insert(std::make_pair(newsockfd, tcp));
                 if (newsockfd < 0) {
@@ -63,20 +65,23 @@ namespace http{
             
             std::string build_response(){
                 // Insert html page or ...
-                    std::ostringstream response; //create the output string stream
-                    response << "HTTP/1.1 200 OK\r\n";
-                    response << "Content-Type: text/html; charset=UTF-8\r\n";
-                    response << "\r\n";
-                    response << "<html><body><h1>Hello younes </h1>";
-                    response << "<h1>from HTTP server!</h4>";
-                    response << "</body></html>";
-                    std::string response_str = response.str();
-                    return (response_str);
+                std::ostringstream response; //create the output string stream
+                
+                response << "HTTP/1.1 200 OK\r\n";
+                response << "Content-Type: text/html; charset=UTF-8\r\n";
+                response << "\r\n";
+                response << "<html><body><h1>Hello younes </h1>";
+                response << "<h1>from HTTP server!</h4>";
+                response << "</body></html>";
+                
+                std::string response_str = response.str();
+                return (response_str);
             }
     
             void send_response(int newsockfd){
                 long byte_send;
                 std::string response = build_response();
+                
                 byte_send = send(newsockfd, response.c_str(), response.length(), 0);
                 if (byte_send > 0){
                     print_message("Server Response sent to client");
@@ -88,8 +93,9 @@ namespace http{
             void run() {
             // Create a set of file descriptors to monitor with select
                 fd_set master_fds;
-                FD_ZERO(&master_fds);
                 std::vector<http::tcpServer>::iterator it = other_sock.begin();
+        
+                FD_ZERO(&master_fds);
                 while (it != other_sock.end()){
                     http::tcpServer& sock = *it;
                     FD_SET(sock.sockfd, &master_fds);
@@ -99,9 +105,9 @@ namespace http{
                 while (true) {
                     // Create a copy of the master set to pass to select
                     fd_set read_fds = master_fds;
-                    
                     // Wait for activity on any of the monitored sockets
                     int activity = select(FD_SETSIZE, &read_fds, NULL, NULL, NULL);
+                    
                     if (activity < 0) {
                         exit_withError("select");
                     }
@@ -122,10 +128,12 @@ namespace http{
                     }
                 }
                 }
+                
                 void read_request(int newsockfd){
                     // Read incoming request data
                     char buffer[1024];
                     int  bytes_received = read(newsockfd, buffer, sizeof(buffer));
+                    
                     if (bytes_received == -1) {
                         close(newsockfd);
                         exit_withError("Failed to read from socket");
@@ -134,6 +142,7 @@ namespace http{
                     std::string request_str(buffer, bytes_received);
                     std::cout<<request_str<<std::endl;
                 }
+                
                 void print_message(const std::string &message){
                         std::cout << message << std::endl;
                 }
--- svr_rsc/main.cpp
+++ svr_rsc/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 19:21:09 by yismaili          #+#    #+#             */
-/*   Updated: 2023/03/30 17:18:08 by yismaili         ###   ########.fr       */
+/*   Updated: 2023/03/30 18:44:21 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int main() {
          vtr.push_back(8000 + i);
          i++;
     }
-  
     http::http_sever server1(vtr, "127.0.0.1");
     server1.run();
     return 0;
