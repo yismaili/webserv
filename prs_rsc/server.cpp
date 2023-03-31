e095d9f0db604c98190404c3245fa7037a1be5eb
Soufiane Nouae <snouae@e2r3p9.1337.ma>
2023-03-31 05:47:54 +0000

    soon

--- prs_rsc/server.cpp
+++ prs_rsc/server.cpp
@@ -23,6 +23,49 @@ std::string toLower(std::string str) {
 //     return count;
 // }
 
+bool isIpAddress(const std::string& ip) {
+    int numDots = 0;
+    std::string temp = "";
+
+    for (int i = 0; i < ip.size(); i++)
+    {
+        if (ip[i] == '.') 
+        {
+            numDots++;
+            if (numDots > 3)
+                return 0;
+            if (temp.size() == 0 || std::stoi(temp) > 255)
+                return 0;
+            temp = "";
+        } 
+        else if (!isdigit(ip[i])) 
+            return 0;
+        else 
+            temp += ip[i];
+    }
+    if (temp.size() == 0 || std::stoi(temp) > 255 || numDots != 3) 
+        return 0;
+    return 1;
+}
+
+int isDomainName(const std::string& domainName)
+{
+
+    if (domainName.empty())
+        return 0;
+    for (char c : domainName) 
+    {
+        if (!std::isalnum(c) && c != '-' && c != '.') 
+            return 0;
+    }
+
+    if (domainName.front() == '-' || domainName.back() == '-')
+        return 0;
+    if (domainName.find('.') == std::string::npos) 
+        return 0;
+    return 1;
+}
+
 int search_char(std::string str, char c)
 {
     int i = 0;
@@ -71,7 +114,7 @@ int is_number(const std::string& str)
     return (1);
 }
 
-int ft_port(std::string value, std::string line)
+int ft_number(std::string value, std::string line)
 {
     int port;
     if (!is_number(value))
@@ -88,13 +131,50 @@ int ft_port(std::string value, std::string line)
         std::cerr << " ERROOR : " << line << std::endl;
         exit (1);
     }
-    return (port)
+    return (port);
+}
+
+void ft_error(std::string line, std::string err)
+{
+    std::cerr << err <<" : " << line << std::endl;
+    exit (1);
+}
+
+void is_empty_value(std::string value, std::string line)
+{
+        if (value.empty())
+            ft_error(line, "Error");
+}
+
+int ft_numbers_value(std::istringstream &iss)
+{
+    int i = 0;
+    std::string value;
+    while (iss >> value)
+        i++;
+    return (i);
+}
+
+int ft_non_alphabetic(std::string value)
+{
+    int i = 0;
+    while (value[i])
+    {
+        if (!isalpha(value[i]))
+            return (1);
+        i++;
+    }
+    return (0);
 }
 
 server::server(Data_config data)
 {
     std::istringstream ss(data.data_server);
     std::string line;
+    int c_server_name = 0;
+    int c_host = 0;
+    int c_root = 0;
+    int c_client_max_body_size = 0;
     while (getline(ss, line))
     {
         if(line.empty() || line[0] == '#')
@@ -106,45 +186,86 @@ server::server(Data_config data)
                 std::cerr << "missing ; in " << line << std::endl;
                 exit (1);
             }
-            line.erase(line.size() - 1)
+            line.erase(line.size() - 1);
         }
         std::string key, value;
-        std::istringstream iss(line) ;
+        std::istringstream iss(line);
         iss >> key >> value;
         //std::cout << key << "|" << "\n";
         key = toLower(key);
         if (key == "server_name")
+        {
+            if(c_server_name)
+                ft_error(line, "Duplicated");
+            is_empty_value(value, line);
+            if (ft_numbers_value(iss) || ft_non_alphabetic(value))
+                ft_error(line, "Error");
+            c_server_name++;
             _server_name.push_back(value);
+        }
         else if (key == "host")
+        {
+            if(c_host)
+                ft_error(line, "Duplicated");
+            is_empty_value(value, line);
+            if (ft_numbers_value(iss) || (!isDomainName(value) && !isIpAddress(value)))
+                ft_error(line, "Error");
+            c_host++;
             _host = value;
+        }
         else if (key == "listen")
         {
-            if (value.empty())
+            is_empty_value(value, line);
+            int port = ft_number(value, line);
+            if (port < 1 || port > 65535)
             {
                 std::cerr << " ERROOR : " << line << std::endl;
                 exit (1);
             }
-            int port = ft_port(value, line);
             _listen.push_back(port);
             while (iss >> value)
             {
-                int port = ft_port(value, line);
+                int port = ft_number(value, line);
+                if (port < 1024 || port > 65535)
+                {
+                    std::cerr << " ERROOR : " << line << std::endl;
+                    exit (1);
+                }
                 _listen.push_back(port);
             }
         }
+
         else if (key == "root")
+        {
+            if(c_root)
+                ft_error(line, "Duplicated");
+            is_empty_value(value, line);
+            if (ft_numbers_value(iss))
+                ft_error(line, "Error");
+            c_root++;
             _root = value;
+        }
+
         else if (key == "client_max_body_size")
-            _client_max_body_size = std::stoi(value);
+        {
+            is_empty_value(value, line);
+            if (ft_numbers_value(iss) || c_client_max_body_size)
+                ft_error(line, "Error");
+            int size = ft_number(value, line);
+            if (size < 0)
+                ft_error(line, "Error size is negative");
+            c_client_max_body_size++;
+            _client_max_body_size = size;
+        }
         else if (key == "error_page")
         {
-            int error_code = std::stoi(value);
+            int error_code = ft_number(value, line);
+            is_empty_value(value, line);
             iss >> value;
+            is_empty_value(value, line);
+            if (ft_numbers_value(iss) )
+                ft_error(line, "Error");
             _error_page[error_code] = value;
-        }
-        else if (key == "meme_types")
-        {
-
         }
         else if (key == "index")
         {
